90e35143185bd9f0a2acc25ea05e3502e404ff61
choiminkyung <choialsrud2002@gmail.com>
2024-02-26 16:24:10 +0900

    project 생성

---
+++ VoiceDiary/VoiceDiary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VoiceDiary
+//
+//  Created by 최민경 on 2/26/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
