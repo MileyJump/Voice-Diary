af3ae457dc45584efe3862c5c73acfe865fe7e5d
choiminkyung <choialsrud2002@gmail.com>
2024-03-14 22:30:46 +0900

    리스트 화면 UI

--- VoiceDiary/VoiceDiary/Views/ListView.swift
+++ VoiceDiary/VoiceDiary/Views/ListView.swift
@@ -10,12 +10,84 @@ import UIKit
 
 class ListView: UIView {
     
+    let allButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("전체", for: .normal)
+        button.titleLabel?.textColor = .white
+        button.titleLabel?.font = UIFont(name: "KCC-Ganpan", size: 15)
+        button.backgroundColor = UIColor(hexCode: "#604937")
+        button.layer.cornerRadius = 8
+        return button
+    }()
+    
+    let meButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("나", for: .normal)
+        button.titleLabel?.textColor = .white
+        button.titleLabel?.font = UIFont(name: "KCC-Ganpan", size: 15)
+        button.backgroundColor = UIColor(hexCode: "#604937").withAlphaComponent(0.3)
+        button.layer.cornerRadius = 8
+        button.sizeToFit()
+        return button
+    }()
+    
+    let youButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("상대방", for: .normal)
+        button.titleLabel?.textColor = .white
+        button.titleLabel?.font = UIFont(name: "KCC-Ganpan", size: 15)
+        button.backgroundColor = UIColor(hexCode: "#604937").withAlphaComponent(0.3)
+        button.layer.cornerRadius = 8
+        return button
+    }()
+    
+    let buttonStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.spacing = 12
+        stackView.distribution = .equalSpacing
+        return stackView
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.backgroundColor = Color.mainBackgroundcolor
+        setUpUI()
+        
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setUpUI(){
+        buttonStackView.addArrangedSubview(allButton)
+        buttonStackView.addArrangedSubview(meButton)
+        buttonStackView.addArrangedSubview(youButton)
+        
+        self.addSubview(buttonStackView)
+        
+        allButton.snp.makeConstraints { make in
+            make.width.equalTo(42)
+            make.height.equalTo(21)
+        }
+//        
+        meButton.snp.makeConstraints { make in
+            make.width.equalTo(28)
+            make.height.equalTo(21)
+        }
+//        
+        youButton.snp.makeConstraints { make in
+            make.width.equalTo(52)
+            make.height.equalTo(21)
+        }
+//        
+        buttonStackView.snp.makeConstraints { make in
+            make.top.equalTo(100)
+            make.centerX.equalToSuperview()
+            
+        }
+        
+        
     }
 }
